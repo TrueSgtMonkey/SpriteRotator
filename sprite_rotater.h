e553520f4a278f1380eb56d5a5f3ab5226f741a1
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2022-01-14 13:52:17 -0800

    fixed indentation

--- sprite_rotater.h
+++ sprite_rotater.h
@@ -12,33 +12,33 @@
 
 class SpriteRotater : public Reference
 {
-	GDCLASS(SpriteRotater, Reference);
-	
-	float distance(Vector3 vec);
-	float angles[3] = { 22.5, 67.5, 90 };
-	//	# Our angle derived from our vector passed in
-	float ang = 0.0;
-	int frame = 0;
-	enum direction {EAST, NORTHEAST, NORTH, NORTHWEST, 
-					WEST, SOUTHWEST, SOUTH, SOUTHEAST};
-	
-	protected:
-		static void _bind_methods();
-		
-	public:
-		int rotationOffset(float rot, int frame);
-		int frameChanger(int frame, int subtractor, int minFrame, int maxFrame);
-		int adjustFrame(Vector3 dir, int frame, float rotation, int subtractor = 2);
-		float getRotation(float rotation);
-		int rotater(Vector3 vec);
-		int get2DVectorFrame(Vector2 vec);
-		int get3DVectorFrame(Vector3 vec);
-		Vector2 get2DFrameVector(int frame);
-		Vector3 get3DFrameVector(int frame);
-		Vector3 setPerpendicular(Vector3 vec, int dir);
-		Vector3 setPerpendicular3D(Vector3 vec, int dir1, int axis);
-		Vector3 setDiagonal(Vector3 vec, int dir, bool norm = true);
-		Vector3 setDiagonal3D(Vector3 vec, int dir, int axis, bool norm = true);
+    GDCLASS(SpriteRotater, Reference);
+    
+    float distance(Vector3 vec);
+    float angles[3] = { 22.5, 67.5, 90 };
+    //    # Our angle derived from our vector passed in
+    float ang = 0.0;
+    int frame = 0;
+    enum direction {EAST, NORTHEAST, NORTH, NORTHWEST, 
+                    WEST, SOUTHWEST, SOUTH, SOUTHEAST};
+    
+    protected:
+        static void _bind_methods();
+        
+    public:
+        int rotationOffset(float rot, int frame);
+        int frameChanger(int frame, int subtractor, int minFrame, int maxFrame);
+        int adjustFrame(Vector3 dir, int frame, float rotation, int subtractor = 2);
+        float getRotation(float rotation);
+        int rotater(Vector3 vec);
+        int get2DVectorFrame(Vector2 vec);
+        int get3DVectorFrame(Vector3 vec);
+        Vector2 get2DFrameVector(int frame);
+        Vector3 get3DFrameVector(int frame);
+        Vector3 setPerpendicular(Vector3 vec, int dir);
+        Vector3 setPerpendicular3D(Vector3 vec, int dir1, int axis);
+        Vector3 setDiagonal(Vector3 vec, int dir, bool norm = true);
+        Vector3 setDiagonal3D(Vector3 vec, int dir, int axis, bool norm = true);
 };
 
 #endif // SPRITE_ROTATER_H
